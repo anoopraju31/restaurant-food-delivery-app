9fcdf80de3a4abdfef58d28638fa6882cf04ac22
anoopraju31 <anoopraju31@gmail.com>
2023-08-01 13:16:05 +0530

    Menu component - menu container styled

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import { menuLinks } from '@/utills/links'
+import Link from 'next/link'
+import { BiMenuAltLeft } from 'react-icons/bi'
 
 const Menu = () => {
-	return <div>Menu</div>
+	// ! Temporary User
+	const user = null
+	return (
+		<div>
+			{/* Menu Button */}
+			<div className='text-3xl'>
+				<BiMenuAltLeft />
+			</div>
+
+			<div className='absolute left-0 top-24 w-full h-[var(--menu-height-sm)] bg-red-500 text-white text-3xl flex flex-col gap-8 justify-center items-center z-10'>
+				{menuLinks.map((item) => (
+					<Link href={item.url} key={item.id}>
+						{item.title}
+					</Link>
+				))}
+
+				<Link href={user ? '/orders' : 'login'}>
+					{user ? 'Orders' : 'Login'}
+				</Link>
+				<Link href='/cart'>CartIcon</Link>
+			</div>
+		</div>
+	)
 }
 
 export default Menu
---
+++ src/utills/links.tsx
@@ -0,0 +1,6 @@
+export const menuLinks = [
+	{ id: 1, title: 'Homepage', url: '/' },
+	{ id: 2, title: 'Menu', url: '/menu' },
+	{ id: 3, title: 'Working Hours', url: '/' },
+	{ id: 4, title: 'Contact', url: '/' },
+]
