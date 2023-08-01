21683713b9cd279fa8a81ba3b1d221bd58c84a69
anoopraju31 <anoopraju31@gmail.com>
2023-08-01 13:56:11 +0530

    Footer component - styled

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,15 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
-	return <div>Footer</div>
+	return (
+		<footer className='h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-red-500 border-t-2 border-t-red-500 flex items-center justify-between'>
+			<Link href='/' className='font-bold text-xl uppercase'>
+				cascio
+			</Link>
+			<p>© ALL RIGHTS RESERVED.</p>
+		</footer>
+	)
 }
 
 export default Footer
