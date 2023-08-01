0747055ad5080d829c3bb229772771a29883fa28
anoopraju31 <anoopraju31@gmail.com>
2023-08-01 13:29:58 +0530

    Menu component - toggle functionality added

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,32 +1,41 @@
+'use client'
+
+import { useState } from 'react'
 import Link from 'next/link'
 import { BiMenuAltLeft } from 'react-icons/bi'
+import { IoCloseOutline } from 'react-icons/io5'
+
 import { menuLinks } from '@/utills/links'
 import { CartIcon } from '.'
 
 const Menu = () => {
+	const [open, setOpen] = useState(false)
+
 	// ! Temporary User
 	const user = null
 	return (
 		<div>
 			{/* Menu Button */}
-			<div className='text-3xl'>
-				<BiMenuAltLeft />
+			<div className='text-3xl' onClick={() => setOpen((prev) => !prev)}>
+				{!open ? <BiMenuAltLeft /> : <IoCloseOutline />}
 			</div>
 
-			<div className='absolute left-0 top-24 w-full h-[var(--menu-height-sm)] bg-red-500 text-white text-3xl flex flex-col gap-8 justify-center items-center z-10'>
-				{menuLinks.map((item) => (
-					<Link href={item.url} key={item.id}>
-						{item.title}
+			{open ? (
+				<div className='absolute left-0 top-24 w-full h-[var(--menu-height-sm)] bg-red-500 text-white text-3xl flex flex-col gap-8 justify-center items-center z-10'>
+					{menuLinks.map((item) => (
+						<Link href={item.url} key={item.id}>
+							{item.title}
+						</Link>
+					))}
+
+					<Link href={user ? '/orders' : 'login'}>
+						{user ? 'Orders' : 'Login'}
 					</Link>
-				))}
-
-				<Link href={user ? '/orders' : 'login'}>
-					{user ? 'Orders' : 'Login'}
-				</Link>
-				<Link href='/cart'>
-					<CartIcon />
-				</Link>
-			</div>
+					<Link href='/cart'>
+						<CartIcon />
+					</Link>
+				</div>
+			) : null}
 		</div>
 	)
 }
