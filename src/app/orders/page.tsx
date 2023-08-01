7893e62816a8c967e53e064aeb6026e84747b74a
anoopraju31 <anoopraju31@gmail.com>
2023-08-01 17:39:28 +0530

    Orders page - styled

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,7 +1,87 @@
 import React from 'react'
 
 const page = () => {
-	return <div>page</div>
+	return (
+		<section className='p-4 lg:px-20 xl:px-40 py-10'>
+			<table className='w-full border-2 border-red-50 border-b-0'>
+				<thead>
+					<tr className='text-left'>
+						<th className='hidden md:block py-5 px-4'>Order ID</th>
+						<th className='py-5 px-4'>Date</th>
+						<th className='py-5 px-4'>Price</th>
+						<th className='hidden md:block py-5 px-4'>Products</th>
+						<th className='py-5 px-4'>Status</th>
+					</tr>
+				</thead>
+
+				<tbody>
+					<tr className='text-sm md:text-base odd:bg-red-50 even:bg-white'>
+						<td className='hidden md:block py-6 px-4'>1237861238721</td>
+						<td className='py-6 px-4'>19.07.2023</td>
+						<td className='py-6 px-4'>89.90</td>
+						<td className='hidden md:block py-6 px-4'>
+							Big Burger Menu (2), Veggie Pizza (2), Coca Cola 1L (2)
+						</td>
+						<td className='py-6 px-4'>On the way (approx. 10min)...</td>
+					</tr>
+					<tr className='text-sm md:text-base odd:bg-red-50 even:bg-white'>
+						<td className='hidden md:block py-6 px-4'>1237861238721</td>
+						<td className='py-6 px-4'>19.07.2023</td>
+						<td className='py-6 px-4'>89.90</td>
+						<td className='hidden md:block py-6 px-4'>
+							Big Burger Menu (2), Veggie Pizza (2), Coca Cola 1L (2)
+						</td>
+						<td className='py-6 px-4'>On the way (approx. 10min)...</td>
+					</tr>
+					<tr className='text-sm md:text-base odd:bg-red-50 even:bg-white'>
+						<td className='hidden md:block py-6 px-4'>1237861238721</td>
+						<td className='py-6 px-4'>19.07.2023</td>
+						<td className='py-6 px-4'>89.90</td>
+						<td className='hidden md:block py-6 px-4'>
+							Big Burger Menu (2), Veggie Pizza (2), Coca Cola 1L (2)
+						</td>
+						<td className='py-6 px-4'>On the way (approx. 10min)...</td>
+					</tr>
+					<tr className='text-sm md:text-base odd:bg-red-50 even:bg-white'>
+						<td className='hidden md:block py-6 px-4'>1237861238721</td>
+						<td className='py-6 px-4'>19.07.2023</td>
+						<td className='py-6 px-4'>89.90</td>
+						<td className='hidden md:block py-6 px-4'>
+							Big Burger Menu (2), Veggie Pizza (2), Coca Cola 1L (2)
+						</td>
+						<td className='py-6 px-4'>On the way (approx. 10min)...</td>
+					</tr>
+					<tr className='text-sm md:text-base odd:bg-red-50 even:bg-white'>
+						<td className='hidden md:block py-6 px-4'>1237861238721</td>
+						<td className='py-6 px-4'>19.07.2023</td>
+						<td className='py-6 px-4'>89.90</td>
+						<td className='hidden md:block py-6 px-4'>
+							Big Burger Menu (2), Veggie Pizza (2), Coca Cola 1L (2)
+						</td>
+						<td className='py-6 px-4'>On the way (approx. 10min)...</td>
+					</tr>
+					<tr className='text-sm md:text-base odd:bg-red-50 even:bg-white'>
+						<td className='hidden md:block py-6 px-4'>1237861238721</td>
+						<td className='py-6 px-4'>19.07.2023</td>
+						<td className='py-6 px-4'>89.90</td>
+						<td className='hidden md:block py-6 px-4'>
+							Big Burger Menu (2), Veggie Pizza (2), Coca Cola 1L (2)
+						</td>
+						<td className='py-6 px-4'>On the way (approx. 10min)...</td>
+					</tr>
+					<tr className='text-sm md:text-base odd:bg-red-50 even:bg-white'>
+						<td className='hidden md:block py-6 px-4'>1237861238721</td>
+						<td className='py-6 px-4'>19.07.2023</td>
+						<td className='py-6 px-4'>89.90</td>
+						<td className='hidden md:block py-6 px-4'>
+							Big Burger Menu (2), Veggie Pizza (2), Coca Cola 1L (2)
+						</td>
+						<td className='py-6 px-4'>On the way (approx. 10min)...</td>
+					</tr>
+				</tbody>
+			</table>
+		</section>
+	)
 }
 
 export default page
