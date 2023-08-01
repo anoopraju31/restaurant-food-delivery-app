0553f1454dc885c4e5109d94c6188cbde64b2444
anoopraju31 <anoopraju31@gmail.com>
2023-08-01 14:26:20 +0530

    Home page: Slider component - styled

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,7 +1,24 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Slider = () => {
-	return <div>Slider</div>
+	return (
+		<section className='h-[var(--slider-height-sm)] md:h-[var(--slider-height-lg)] flex flex-col lg:flex-row bg-red-50'>
+			{/* Text Container */}
+			<div className='flex-1 flex items-center justify-center flex-col gap-8 text-red-500 font-bold'>
+				<h1 className='p-4 md:p-10 text-5xl md:text-6xl xl:text-7xl text-center uppercase '>
+					{' '}
+					title{' '}
+				</h1>
+				<button className='py-4 px-8 bg-red-500 text-white'> Order Now </button>
+			</div>
+
+			{/* Image Container */}
+			<div className='w-full flex-1 relative'>
+				<Image src='/slider/slide1.png' alt='' fill className='object-cover' />
+			</div>
+		</section>
+	)
 }
 
 export default Slider
