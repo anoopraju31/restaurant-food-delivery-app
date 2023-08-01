8f13c6878f122173387ce6820ab1d6d08e7799e7
anoopraju31 <anoopraju31@gmail.com>
2023-08-01 13:22:15 +0530

    Cart Icon component - styled

--- src/components/CartIcon.tsx
+++ src/components/CartIcon.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import { BsFillBasket3Fill } from 'react-icons/bs'
 
 const CartIcon = () => {
-	return <div>CartIcon</div>
+	return (
+		<div className='flex items-center gap-4'>
+			<div className='relative w-8 h-8 md:w-5 md:h-5'>
+				<BsFillBasket3Fill />
+			</div>
+			<span>Cart (0)</span>
+		</div>
+	)
 }
 
 export default CartIcon
