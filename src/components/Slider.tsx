11fd8e791a2d668f8471de1e09b33244011d884f
anoopraju31 <anoopraju31@gmail.com>
2023-08-01 14:35:46 +0530

    Home page: Slider component - slider functionality added

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,21 +1,41 @@
+'use client'
+
+import { useEffect, useState } from 'react'
 import Image from 'next/image'
-import React from 'react'
+import { slideData } from '@/utills/slides'
 
 const Slider = () => {
+	const [currentSlide, setCurrentSlide] = useState(0)
+
+	useEffect(() => {
+		const interval = setInterval(
+			() => setCurrentSlide((prev) => (prev + 1) % slideData.length),
+			4000,
+		)
+
+		return () => {
+			clearInterval(interval)
+		}
+	}, [])
+
 	return (
 		<section className='h-[var(--slider-height-sm)] md:h-[var(--slider-height-lg)] flex flex-col lg:flex-row bg-red-50'>
 			{/* Text Container */}
 			<div className='flex-1 flex items-center justify-center flex-col gap-8 text-red-500 font-bold'>
-				<h1 className='p-4 md:p-10 text-5xl md:text-6xl xl:text-7xl text-center uppercase '>
-					{' '}
-					title{' '}
+				<h1 className='p-4 md:p-10 text-5xl md:text-6xl xl:text-7xl text-center uppercase'>
+					{slideData[currentSlide].title}
 				</h1>
 				<button className='py-4 px-8 bg-red-500 text-white'> Order Now </button>
 			</div>
 
 			{/* Image Container */}
 			<div className='w-full flex-1 relative'>
-				<Image src='/slider/slide1.png' alt='' fill className='object-cover' />
+				<Image
+					src={slideData[currentSlide].image}
+					alt=''
+					fill
+					className='object-cover'
+				/>
 			</div>
 		</section>
 	)
---
+++ src/utills/slides.ts
@@ -0,0 +1,17 @@
+export const slideData = [
+	{
+		id: 1,
+		title: 'always fresh & always crispy & always hot',
+		image: '/slider/slide1.png',
+	},
+	{
+		id: 2,
+		title: 'we deliver your order wherever you are in NY',
+		image: '/slider/slide2.png',
+	},
+	{
+		id: 3,
+		title: 'the best pizza to share with your family',
+		image: '/slider/slide3.jpg',
+	},
+]
