c21bf0b901598be89104e1032760a6a41036b1ef
anoopraju31 <anoopraju31@gmail.com>
2023-08-01 12:57:30 +0530

    Navbar component - mobile view initial

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,18 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
-	return <div>Navbar</div>
+	return (
+		<header className='w-full h-12 p-4 flex items-center justify-between border-b-2 border-b-red-500 text-red-500 uppercase'>
+			{/* Logo */}
+			<div className='text-xl'>
+				<Link href='/'> Cacio </Link>
+			</div>
+
+			{/* Menu */}
+			<div>Menu</div>
+		</header>
+	)
 }
 
 export default Navbar
